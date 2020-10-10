075dad64f52340ea196e2fcac0d85327cc2e0024
syed <umaranwar707@gmail.com>
2020-10-10 11:12:38 +0530

    Added:double combination and cal percentage

--- flip_coin_simulation.sh
+++ flip_coin_simulation.sh
@@ -1,25 +1,41 @@
 #!/bin/bash -x
 HEAD=1
 TAIL=0
-headc=0
-tailc=0
+headhead=0
+tailtail=0
+headtail=0
+tailhead=0
 declare -A dic
 for ((i=0;i<20;i++))
 do
 var=$((RANDOM%2))
-if [ $var -eq $HEAD ]
-then 
-	((headc++))
-else
-	((tailc++))
-
-fi
+var1=$((RANDOM%2))
+double=$var$var1
+case $double in
+	$HEAD$HEAD)
+			((headhead++));;
+	$HEAD$TAIL)
+			((headtail++));;
+	$TAIL$HEAD)
+			((tailhead++));;
+	$TAIL$TAIL)
+			((tailtail++));;
+esac
 done
-dic[head]=$headc
-dic[tail]=$tailc
+dic[HT]=$headtail
+dic[TT]=$tailtail
+dic[TH]=$tailhead
+dic[HH]=$headhead
 echo ${dic[@]}
-hper=`echo $headc | awk '{print ($1/20)*100}'`
-echo "the head percentage is $hper%"
-tper=`echo $tailc | awk '{print ($1/20)*100}'`
-echo "the tail percentage is $tper%"
+echo ${!dic[@]}
+hper=`echo $headhead | awk '{print ($1/20)*100}'`
+echo "the HH percentage is $hper%"
+tper=`echo $tailtail | awk '{print ($1/20)*100}'`
+echo "the TT percentage is $tper%"
+thper=`echo $tailhead | awk '{print ($1/20)*100}'`
+echo "the TH percentage is $thper%"
+htper=`echo $headtail | awk '{print ($1/20)*100}'`
+echo "the HT percentage is $htper%"
+
+
 
